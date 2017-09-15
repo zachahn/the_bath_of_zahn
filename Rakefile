12fbdbb5cdac12cdc6c57c3d06966148f4a35d24
Zach Ahn <engineering@zachahn.com>
2017-09-15 07:51:33 -0400

    Add code to extract styleguide rubocop rules

--- Rakefile
+++ Rakefile
@@ -9,4 +9,13 @@ task :fix do
   system %(rubocop -c rubocop.yml -a)
 end
 
+task :extract do
+  require "yaml"
+  require_relative "lib/the_bath_of_zahn/utility/extractor"
+
+  extractor = TheBathOfZahn::Utility::Extractor.new
+
+  File.write("config/style_guide.yml", extractor.rules_styleguide.to_yaml)
+end
+
 task default: :test
---
+++ lib/the_bath_of_zahn/utility/extractor.rb
@@ -0,0 +1,38 @@
+module TheBathOfZahn
+  module Utility
+    class Extractor
+      def rules_settings
+        @rules_settings ||=
+          begin
+            path = File.join(path_to_rubocop, "config", "default.yml")
+            YAML.load_file(path)
+          end
+      end
+
+      def rules_enabled
+        @rules_enabled ||=
+          begin
+            path = File.join(path_to_rubocop, "config", "enabled.yml")
+            YAML.load_file(path)
+          end
+      end
+
+      def rules_styleguide
+        @rules_styleguide ||=
+          rules_enabled.select { |_, v| v["StyleGuide"] }
+            .map { |name, config| [name, config.merge(settings_for(name))] }
+            .to_h
+      end
+
+      private
+
+      def settings_for(name)
+        rules_settings[name] || {}
+      end
+
+      def path_to_rubocop
+        `bundle show rubocop`.chomp
+      end
+    end
+  end
+end
